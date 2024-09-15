bfa58f3e0908e92b18b902aa52e7eca50fe074dd
SaaD036 <shaad036@gmail.com>
2024-09-16 01:03:52 +0600

    Update user model with 3 additional field: account status, reset password token and account edit history

--- src/models/user.js
+++ src/models/user.js
@@ -1,59 +1,86 @@
-
 'use strict';
 const { Sequelize, Model } = require('sequelize');
 
 const { UserRole } = require('../constants/roles');
+const { UserAccountStatus } = require('../constants/users');
 
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {}
 
-    User.init({
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            field: 'first_name',
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            field: 'last_name',
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
+    User.init(
+        {
+            firstName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: 'first_name',
+            },
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: 'last_name',
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: true,
+                },
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            role: {
+                type: DataTypes.STRING,
+                defaultValue: UserRole.USER,
+                validate: {
+                    isIn: [UserRole.ADMIN, UserRole.USER, UserRole.VISITOR],
+                },
+            },
+            accountStatus: {
+                type: DataTypes.STRING,
+                defaultValue: UserAccountStatus.WAITING_FOR_USER_APPROVAL,
+                allowNull: false,
+                validate: {
+                    isIn: [
+                        UserAccountStatus.ACTIVE,
+                        UserAccountStatus.DEACTIVE,
+                        UserAccountStatus.DELETED,
+                        UserAccountStatus.WAITING_FOR_USER_APPROVAL,
+                    ],
+                },
+                field: 'account_status',
+            },
+            resetPasswordToken: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                field: 'reset_password_token',
+            },
+            accountEditHistory: {
+                type: DataTypes.JSON,
+                allowNull: true,
+                field: 'account_edit_history',
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                defaultValue: Sequelize.fn('NOW'),
+                field: 'created_at',
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                defaultValue: Sequelize.fn('NOW'),
+                field: 'updated_at',
             },
         },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        role: {
-            type: DataTypes.STRING,
-            defaultValue: UserRole.USER,
-            validate: {
-                isIn: [UserRole.ADMIN,  UserRole.USER, UserRole.VISITOR],
-            },
-        },
-        createdAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
-            defaultValue: Sequelize.fn('NOW'),
-            field: 'created_at',
-        },
-        updatedAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
-            defaultValue: Sequelize.fn('NOW'),
-            field: 'updated_at',
-        },
-    }, {
-        sequelize,
-        modelName: 'User',
-        tableName: 'users',
-    });
+        {
+            sequelize,
+            modelName: 'User',
+            tableName: 'users',
+        }
+    );
 
     return User;
 };
