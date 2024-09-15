ab4c4c36cc37acd6ce92675cebc5e96e9f5f1b7c
SaaD036 <shaad036@gmail.com>
2024-09-16 03:13:57 +0600

    Add forgot password routes validator system

--- src/routes/auth/authValidators/index.js
+++ src/routes/auth/authValidators/index.js
@@ -1,14 +1,17 @@
 const { check } = require('express-validator');
 
 const loginValidator = [
-    check('email')
-        .isEmail()
-        .withMessage('Email is required and should be valid.'),
+    check('email').isEmail().withMessage('Email is required and should be valid.'),
     check('password')
-        .custom(value =>  (value || '').length > 0)
+        .custom((value) => (value || '').length > 0)
         .withMessage('Password is required.'),
+];
+
+const forgotPasswordValidator = [
+    check('email').isEmail().withMessage('Email is required and should be valid.'),
 ];
 
 module.exports = {
     loginValidator,
+    forgotPasswordValidator,
 };
