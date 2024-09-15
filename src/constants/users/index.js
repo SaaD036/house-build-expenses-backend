478131e9e932bb9981c3ea8e632bde2281492dfa
SaaD036 <shaad036@gmail.com>
2024-09-16 00:58:56 +0600

    Create user account status constants

---
+++ src/constants/users/index.js
@@ -0,0 +1,10 @@
+const UserAccountStatus = {
+    ACTIVE: 'active',
+    DEACTIVE: 'deactive',
+    DELETED: 'deleted',
+    WAITING_FOR_USER_APPROVAL: 'waiting_for_user_approval',
+};
+
+module.exports = {
+    UserAccountStatus,
+};
