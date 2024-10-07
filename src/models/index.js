91b70fd3a1b17255d2d4a25538c15d3ecc4d378d
SaaD036 <shaad036@gmail.com>
2024-10-08 01:07:42 +0600

    Reset all the database codes, files and configs

--- src/models/index.js
+++ src/models/index.js
@@ -4,18 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
-
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'DEV';
-const config = require(__dirname + '/../../database/config/config.js')[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../../database/config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize(
-    process.env[`${env}_DB_NAME`],
-    process.env[`${env}_DB_USER`],
-    process.env[`${env}_DB_PASSWORD`] || '',
-    config
-);
+let sequelize;
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs.readdirSync(__dirname)
     .filter((file) => {
