b38211a92ee8377b29e15d3b05476ad4615c1dc6
SaaD036 <shaad036@gmail.com>
2024-10-17 23:17:21 +0600

    Add filters in get all expenses API

---
+++ src/constants/expense/index.js
@@ -0,0 +1,5 @@
+const EXPENSE_START_DATE = new Date('2024-06-01T00:00:00.000');
+
+module.exports = {
+    EXPENSE_START_DATE,
+};
--- src/controllers/expense/index.js
+++ src/controllers/expense/index.js
@@ -1,14 +1,29 @@
 const { Expenses } = require('../../models');
 
+const { prepareFiltersForAllExpenses } = require('../../queryHelper/expenses');
+
 const { HTTP_STATUS } = require('../../constants/http');
 
 const getAllExpenses = async (req, res, next) => {
     try {
+        const { page, limit, title, description, fromDate, toDate } = req.query;
+        const filter = prepareFiltersForAllExpenses({
+            page,
+            limit,
+            title,
+            description,
+            fromDate,
+            toDate,
+        });
+
         const expenses = await Expenses.findAll({
             include: {
                 association: 'creator',
                 attributes: ['id', 'firstName', 'lastName'],
             },
+            where: filter.where,
+            limit: filter.limit,
+            offset: filter.offset,
         });
 
         return res.status(HTTP_STATUS.OK).json({
