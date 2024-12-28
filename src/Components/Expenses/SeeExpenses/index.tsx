7c0dea3fa27390bfe15b463120e840d7a5306470
SaaD036 <shaad036@gmail.com>
2024-12-29 00:23:58 +0600

    HBE-8: Integrate expense data fetched from backend to the expense page

--- src/Components/Expenses/SeeExpenses/index.tsx
+++ src/Components/Expenses/SeeExpenses/index.tsx
@@ -1,105 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+
+import { MoreVert as ActionColumnIcon } from '@mui/icons-material';
+
 import CustomTable from '../../Custom/CustomTable';
+import TabComponentLoader from '../../Custom/CustomLoadingItems/TabComponentLoader';
 
-const columns = [
-    {
-        key: 'title',
-        label: 'Title',
-        sortable: true,
-    },
-    {
-        key: 'amount',
-        label: 'Amount',
-        sortable: true,
-    },
-    {
-        key: 'description',
-        label: 'Description',
-    },
-    {
-        key: 'expense_time',
-        label: 'Expense time',
-        sortable: true,
-    },
-    {
-        key: 'creator',
-        label: 'Creator',
-    },
-];
+import { getAllExpenses } from '../../../Redux/actions/expenseAction';
 
-const rows = [
-    {
-        title: {
-            value: 'Rod',
-        },
-        amount: {
-            value: 10,
-        },
-        description: {
-            value: 'No description is needed',
-        },
-        expense_time: {
-            value: '2024-12-01',
-        },
-        creator: {
-            value: 'Kader khan',
-        },
-    },
-    {
-        title: {
-            value: 'Cement',
-        },
-        amount: {
-            value: 100,
-        },
-        description: {
-            value: 'No description is needed',
-        },
-        expense_time: {
-            value: '2024-12-01',
-        },
-        creator: {
-            value: 'Kader khan',
-        },
-    },
-    {
-        title: {
-            value: 'Sand',
-        },
-        amount: {
-            value: 3.5,
-        },
-        description: {
-            value: 'No description is needed',
-        },
-        expense_time: {
-            value: '2024-12-01',
-        },
-        creator: {
-            value: 'Kader khan',
-        },
-    },
-    {
-        title: {
-            value: 'Bricks',
-        },
-        amount: {
-            value: 5,
-        },
-        description: {
-            value: 'No description is needed  jsh jhguyg jhgauyfg jshghuyfg ahguyfgds jhsgfuyg',
-        },
-        expense_time: {
-            value: '2024-12-01',
-        },
-        creator: {
-            value: 'Kader khan',
-        },
-    },
-];
+import { EXPENSE_TABLE_COLUMNS } from './constants';
+import { SeeExpensesPropsType } from './interfaces';
+import styles from './styles.module.css';
 
-const SeeExpenses = () => {
-    return <CustomTable columns={columns} rowData={rows} totalRowCount={100} />;
+const SeeExpenses = (props: SeeExpensesPropsType) => {
+    const { expenses, expensesCount, getAllExpenses } = props;
+
+    const [isLoadingExpenseData, setIsLoadingExpenseData] = useState(true);
+
+    const loadExpenseData = async () => {
+        setIsLoadingExpenseData(true);
+        await getAllExpenses(null);
+        setIsLoadingExpenseData(false);
+    };
+
+    const getExpenseTableRows = () => {
+        const expenseTableRows = (expenses || []).map((expense) => ({
+            title: {
+                value: expense.title,
+            },
+            amount: {
+                value: expense.amount,
+            },
+            description: {
+                value: expense.description,
+            },
+            expense_time: {
+                value: expense.expenseAt,
+            },
+            creator: {
+                value: `${expense.creator.firstName} ${expense.creator.lastName}`,
+            },
+            action: {
+                value: (
+                    <ActionColumnIcon
+                        sx={{ color: '#158901' }}
+                        className={styles.actionColumnIcon}
+                    />
+                ),
+            },
+        }));
+
+        return expenseTableRows;
+    };
+
+    useEffect(() => {
+        loadExpenseData();
+    }, []);
+
+    return (
+        <>
+            {isLoadingExpenseData ? (
+                <TabComponentLoader />
+            ) : (
+                <CustomTable
+                    columns={EXPENSE_TABLE_COLUMNS}
+                    rowData={getExpenseTableRows()}
+                    totalRowCount={expensesCount}
+                />
+            )}
+        </>
+    );
 };
 
-export default SeeExpenses;
+const mapStateToProps = (state: any) => ({
+    expenses: state.expense.expenses,
+    expensesCount: state.expense.expensesCount,
+});
+
+const mapDispatchToProps = {
+    getAllExpenses,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SeeExpenses);
