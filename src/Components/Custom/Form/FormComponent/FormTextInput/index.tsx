af04e8716df682e578c2b584cd0e0a3b9c04aa26
SaaD036 <shaad036@gmail.com>
2025-01-06 22:32:07 +0600

    HBE-9: Create text input for form component

---
+++ src/Components/Custom/Form/FormComponent/FormTextInput/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useField } from 'formik';
+
+import { FormTextInputPropsType } from './interfaces';
+import styles from './styles.module.css';
+
+const FormTextInput = (props: FormTextInputPropsType) => {
+    const { id, label, ...fieldHookConfigProps } = props;
+    const [field, meta] = useField(fieldHookConfigProps);
+
+    const hasError = () => {
+        if (id !== field.name) {
+            return false;
+        }
+
+        return !!(meta.touched && meta.error);
+    };
+
+    return (
+        <div className={styles.formTextInputContainer}>
+            <div>{label}</div>
+            <input
+                id={id}
+                className={hasError() ? styles.inputError : styles.formTextInputBox}
+                onChange={field.onChange}
+            />
+            {hasError() ? <div className={styles.errorMessage}>{meta.error}</div> : null}
+        </div>
+    );
+};
+
+export default FormTextInput;
