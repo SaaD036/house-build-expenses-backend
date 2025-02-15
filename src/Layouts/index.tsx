9ffc4f8a98ff76580e41d05a66d032544749248d
SaaD036 <shaad036@gmail.com>
2025-02-16 00:32:39 +0600

    HBE-22: Make sidebar and layout responsive

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -8,10 +8,25 @@ import styles from './styles.module.css';
 const Layout = (props: LayoutPropsTypes) => {
     const { children } = props;
 
+    const renderFooterSection = () => {
+        // eslint-disable-next-line quotes
+        const FOOTER_TEXT = "A SaaD's product";
+
+        return (
+            <div className={`center ${styles.footer}`}>
+                <div className="center">
+                    <span>{FOOTER_TEXT}</span>
+                </div>
+            </div>
+        );
+    };
+
     return (
         <div style={{ display: 'flex' }}>
             <div className={`center ${styles.layout}`}>
+                <div style={{ height: '40px' }}></div>
                 <SideBar />
+                {renderFooterSection()}
             </div>
             <div className={styles.childrenSection}>{children}</div>
         </div>
