de8f3b5bda154d10e10ee11ca09ca8e59d52262b
SaaD036 <shaad036@gmail.com>
2025-08-09 21:51:22 +0600

    HBE-33: Show see expense page when user with user role tries to navigate create expense page with url

--- src/Pages/Expenses/index.tsx
+++ src/Pages/Expenses/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { get } from 'lodash';
+import { jwtDecode } from 'jwt-decode';
 
 import ExpensePageComponent from '../../Components/Expenses/ExpensePageComponent';
 import Header from '../../Components/Header';
@@ -6,15 +8,27 @@ import CustomNavTabs from '../../Components/Custom/CustomTab';
 
 import { useQuery } from '../../Redux/apiServices/buildURL';
 import { getExpensePageTabs } from './utilities';
+import { cookieName, getCookie } from '../../Utilities/Cookies';
 
 import { tabValueItem } from './constants';
 import styles from './styles.module.css';
+import { UserRole } from '../../Constants/Users';
 
 const ExpensesPage = () => {
     const query = useQuery();
     const tabName = query.get('tab') || tabValueItem.SEE_EXPENSES;
 
     const [activeTab, setActiveTab] = useState(tabName);
+
+    useEffect(() => {
+        const userToken = getCookie(cookieName.USER_TOKEN);
+        const user = jwtDecode(userToken || '');
+        const isLoggedInUserAsUser = get(user, 'role', UserRole.USER) === UserRole.USER;
+
+        if (isLoggedInUserAsUser && activeTab === tabValueItem.CREATE_EXPENSE) {
+            setActiveTab(tabValueItem.SEE_EXPENSES);
+        }
+    }, []);
 
     return (
         <div className={`pageComponent center ${styles.expensePageComponent}`}>
