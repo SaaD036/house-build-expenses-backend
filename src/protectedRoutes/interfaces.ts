4b8a6ebc63c06128e0db4a35a9d273c18c94f0ec
SaaD036 <shaad036@gmail.com>
2025-08-09 21:46:30 +0600

    HBE-33: Keep routes persistent when user refreshes the page

--- src/protectedRoutes/interfaces.ts
+++ src/protectedRoutes/interfaces.ts
@@ -2,7 +2,6 @@ import React from 'react';
 
 export type ProtectedRoutePropType = {
     children: React.ReactElement;
-    user: any;
     redirect?: string;
     redirectIfNotLoggedIn?: string;
     auth?: true;
