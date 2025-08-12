0fb59cce2ac0070691aa2704b942ae4ac7474fc5
SaaD036 <shaad036@gmail.com>
2025-08-12 21:29:43 +0600

    HBE-33: Update get all users API to with filter functionality

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,14 +1,17 @@
 const { get } = require('lodash');
+const { literal } = require('sequelize');
 
 const { User } = require('../../models');
 
 const { prepareQueryFilterForAllusers } = require('../../queryHelper/users');
 
 const { HTTP_STATUS } = require('../../constants/http');
+const { UserRole } = require('../../constants/roles');
 
 const getAllUser = async (req, res, next) => {
     try {
         const { page, limit, name, email, role, accountStatus } = req.query;
+        const loggedInUserRole = get(req, 'user.role', UserRole.USER);
         const filter = prepareQueryFilterForAllusers({
             page,
             limit,
@@ -18,16 +21,51 @@ const getAllUser = async (req, res, next) => {
             accountStatus,
         });
 
-        const users = await User.findAndCountAll({
-            where: filter.where,
-            limit: filter.limit,
-            offset: filter.offset,
-            order: ['id'],
+        const [users, totalUsers] = await Promise.all([
+            User.findAll({
+                where: filter.where,
+                limit: filter.limit,
+                offset: filter.offset,
+                include: {
+                    association: 'expenses',
+                    attributes: [],
+                },
+                attributes: [
+                    'id',
+                    'firstName',
+                    'lastName',
+                    'email',
+                    'role',
+                    'accountStatus',
+                    'accountEditHistory',
+                    [
+                        literal(`(
+                        SELECT COUNT("expenses"."id") FROM "expenses"
+                        WHERE "expenses"."created_by" = "User"."id"
+                    )`),
+                        'totalExpenseCount',
+                    ],
+                ],
+                order: [['id', 'ASC']],
+            }),
+            User.count({
+                where: filter.where,
+            }),
+        ]);
+
+        const flatUsers = users.map((user) => user.get({ plain: true }));
+
+        const updatedUsers = (flatUsers || []).map((user) => {
+            if (loggedInUserRole === UserRole.USER) {
+                delete user.totalExpenseCount;
+            }
+
+            return user;
         });
 
         return res.status(HTTP_STATUS.OK).json({
-            users: get(users, 'rows', []),
-            userCount: get(users, 'count', 0),
+            users: updatedUsers || [],
+            totalUsers,
         });
     } catch (error) {
         next(error.message || error);
