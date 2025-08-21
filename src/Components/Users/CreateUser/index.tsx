e735084cf2f4ffe119be17e13fd205d9c58fffc2
SaaD036 <shaad036@gmail.com>
2025-08-22 01:16:53 +0600

    HBE-48: Integrate create user APIs with create user form

--- src/Components/Users/CreateUser/index.tsx
+++ src/Components/Users/CreateUser/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { FormikHelpers } from 'formik';
 
 import { Container, Grid2 } from '@mui/material';
 
@@ -8,6 +10,8 @@ import FormTextInput from '../../Custom/Form/FormComponent/FormTextInput';
 import CardContainer from '../../CardContainer';
 import TabComponentLoader from '../../Custom/CustomLoadingItems/TabComponentLoader';
 import ButtonSection from '../../Custom/CustomButton/ButtonSection';
+
+import { createUser } from '../../../Redux/actions/userAction';
 
 import {
     CREATE_USER_FORM_INITIAL_VALUE,
@@ -22,16 +26,15 @@ import styles from './styles.module.css';
 import FormSelectInput from '../../Custom/Form/FormComponent/FormSelectInput';
 
 const CreateUser = (props: CreateUserPagePropsType) => {
-    const { disabledForm } = props;
+    const { disabledForm, createUser } = props;
 
     const [loading, setLoading] = useState<boolean>();
-    const [createUserFormData, setCreateUserFormData] = useState<CreateUserFormDataType>(
-        CREATE_USER_FORM_INITIAL_VALUE
-    );
 
-    useEffect(() => {
-        console.log('SaaD : ', createUserFormData);
-    }, [createUserFormData]);
+    const onSubmit = async (data: CreateUserFormDataType) => {
+        setLoading(true);
+        await createUser(data);
+        setLoading(false);
+    };
 
     return (
         <div style={{ display: 'grid', gap: '25px' }}>
@@ -47,10 +50,10 @@ const CreateUser = (props: CreateUserPagePropsType) => {
                     <Form
                         initialValue={CREATE_USER_FORM_INITIAL_VALUE}
                         validationObject={CREATE_USER_FORM_VALIDATOR}
-                        onFormValueChange={(data: CreateUserFormDataType) =>
-                            setCreateUserFormData(data)
-                        }
-                        onSubmit={() => {}}
+                        onSubmit={(
+                            formValue: CreateUserFormDataType,
+                            formikHelpers: FormikHelpers<object>
+                        ) => onSubmit(formValue)}
                     >
                         <Grid2 container spacing={3} marginTop={3}>
                             <Grid2 size={{ xs: 12, sm: 6 }}>
@@ -79,7 +82,6 @@ const CreateUser = (props: CreateUserPagePropsType) => {
                                 />
                             </Grid2>
                         </Grid2>
-
                         <ButtonSection className={styles.buttonSection}>
                             <button className="button" type="submit">
                                 SUBMIT
@@ -92,4 +94,8 @@ const CreateUser = (props: CreateUserPagePropsType) => {
     );
 };
 
-export default CreateUser;
+const mapDispatchToProps = {
+    createUser,
+};
+
+export default connect(null, mapDispatchToProps)(CreateUser);
