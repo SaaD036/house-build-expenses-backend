64276cfd95f30b7c924d470ec6213d2772867794
SaaD036 <shaad036@gmail.com>
2025-08-25 23:54:40 +0600

    HBE-53: Implement next url when users logs in

--- src/RoutesProtection/index.tsx
+++ src/RoutesProtection/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 import { getUserFromToken } from '../Utilities/Users/UserToken';
 
@@ -8,13 +8,16 @@ import { ProtectedRoutePropType } from './interfaces';
 const ProtectedRoutes = (props: ProtectedRoutePropType) => {
     const { children, auth, role, redirectIfNotLoggedIn, redirectIfNotAuthenticated, redirect } =
         props;
+
     const user = getUserFromToken();
+    const locations = useLocation();
 
     if (auth) {
         const isRoleAuthenticated = (role || []).includes(user?.role || '');
 
         if (!user) {
-            return <Navigate to={redirectIfNotLoggedIn || '/auth/login'} replace />;
+            const nextURL = `?next=${locations.pathname}${locations.search}`;
+            return <Navigate to={redirectIfNotLoggedIn || `/auth/login${nextURL}`} replace />;
         }
 
         if (role && !isRoleAuthenticated) {
