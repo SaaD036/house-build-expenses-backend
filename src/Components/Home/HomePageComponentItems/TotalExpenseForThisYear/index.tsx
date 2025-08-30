b599d81689d77aaa25acbf354ba18374b47fc182
SaaD036 <shaad036@gmail.com>
2025-08-30 21:46:07 +0600

    HBE-32 - Add the shimmer effect in project and implement in the total expense component

--- src/Components/Home/HomePageComponentItems/TotalExpenseForThisYear/index.tsx
+++ src/Components/Home/HomePageComponentItems/TotalExpenseForThisYear/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ShimmerTitle } from 'shimmer-effects-react';
+
 import CardContainer from '../../../CardContainer';
 
 import styles from './styles.module.css';
@@ -11,9 +13,20 @@ const TotalExpenseForThisYear = () => {
     return (
         <div style={{ height: '100%' }}>
             <CardContainer title={renderTitleForTotalExpense()}>
-                <h2>
-                    <b>20,00,000 BDT</b>
-                </h2>
+                <ShimmerTitle
+                    mode="custom"
+                    line={2}
+                    gap={5}
+                    className={styles.loaderShimmer}
+                    from="#158901"
+                    via="#9ef48fff"
+                    to="#158901"
+                    loading={false}
+                >
+                    <h2>
+                        <b className="text">20,00,000 BDT</b>
+                    </h2>
+                </ShimmerTitle>
             </CardContainer>
         </div>
     );
