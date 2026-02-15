d0e2f91d7eccc73e53392ed378154510cb30d183
SaaD036 <shaad036@gmail.com>
2026-02-16 00:48:37 +0600

    HBE-86: Add logics in the create DO API

--- src/controllers/do/index.js
+++ src/controllers/do/index.js
@@ -1,5 +1,20 @@
+const { DOs } = require('../../models');
+
 const createDO = async (req, res, next) => {
     try {
+        const { shopName, shopAddress, doItem, description, amount, doDate } = req.body;
+        const { id } = req.user;
+
+        await DOs.create({
+            shopName,
+            shopAddress,
+            doItem,
+            description,
+            amount: Number(amount),
+            doDate: doDate ? new Date(doDate) : new Date(),
+            createdBy: id,
+        });
+
         return res.status(200).json({
             message: 'DO created successfully',
         });
