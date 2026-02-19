5e9e17d7a50015910298fa74ff1551222a311f1a
SaaD036 <shaad036@gmail.com>
2026-02-20 03:04:40 +0600

    HBE-89: Integrate fetch DOs API in the redux state

---
+++ src/Redux/actions/doAction.ts
@@ -0,0 +1,79 @@
+/* eslint-disable indent */
+import { Dispatch } from 'react';
+import { Method } from 'axios';
+
+import { initiateToast } from '../../Components/Custom/CustomToast';
+
+import { buildURL } from '../apiServices/buildURL';
+import { callAxiosAPI } from '../apiServices/calAPPI';
+
+import ValidationError from '../../ErrorHandlers/ValidationError';
+
+import { GET_ALL_DOS } from '../types/dos';
+
+import { DOreducerType } from '../reducers/reducerDataType';
+
+import doAPIs from '../apiServices/APIs/doAPIs.json';
+import { HTTP_STATUS_CODE } from '../../Constants/HTTP';
+import { get } from 'lodash';
+import { DO } from '../../Types/DOs';
+
+type DispatchType = { type: string; payload: Partial<DOreducerType> };
+
+export const getAllDOs = (filters: any) => async (dispatch: Dispatch<DispatchType>) => {
+    try {
+        const { path, method } = doAPIs.GET_ALL_DOS;
+        const { UNAUTHENTICATED, INTERNAL_SERVER_ERROR } = HTTP_STATUS_CODE;
+        const params: any = {};
+
+        const URLwithVarsANDparams = buildURL(path, {}, params);
+        const { status, data } = await callAxiosAPI({
+            url: URLwithVarsANDparams,
+            method: method as Method,
+        });
+
+        if (status === UNAUTHENTICATED) {
+            throw new ValidationError('User does not have access to DOs');
+        } else if (status === INTERNAL_SERVER_ERROR) {
+            throw new ValidationError('Can not fetch DOs');
+        }
+
+        const dos: DO[] = get(data, 'do', []).map((value: any) => ({
+            amount: Number(value.amount),
+            createdAt: new Date(value.createdAt),
+            creator: {
+                id: value.creator.id,
+                firstName: value.creator.firstName,
+                lastName: value.creator.lastName,
+            },
+            description: value.description,
+            doDate: new Date(value.doDate),
+            doEditHistory: value.doEditHistory,
+            doItem: value.doItem,
+            id: value.id,
+            imageURL: value.imageURL,
+            shopAddress: {
+                area: value.shopAddress.area,
+                ward: value.shopAddress.ward,
+                upazilla: value.shopAddress.upazilla,
+                district: value.shopAddress.district,
+            },
+            shopName: value.shopName,
+        }));
+
+        dispatch({
+            type: GET_ALL_DOS,
+            payload: {
+                dos,
+                doCount: 1000,
+            },
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            initiateToast({
+                type: 'error',
+                message: error.message.toString(),
+            });
+        }
+    }
+};
