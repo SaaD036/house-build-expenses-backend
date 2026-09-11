5850caa8718318fb3c7fa0f4aa984ac9511c0b18
SaaD036 <shaad036@gmail.com>
2026-09-11 08:53:54 +0600

    HBE-108: (frontend) Add loader to expense-details modal

--- frontend/src/Components/Expenses/ExpenseModal/ExpenseModalDetails.tsx
+++ frontend/src/Components/Expenses/ExpenseModal/ExpenseModalDetails.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
@@ -8,6 +9,7 @@ import { getSingleExpense } from '../../../Redux/actions/expenseAction';
 import styles from './styles.module.css';
 import { ReducerStateType } from '../../../Redux/reducers';
 import { ExpenseModalDetailsPropType } from './interfaces';
+import { ShimmerCardLoader } from '../../Custom/CustomLoadingItems/ShimmerLoader';
 
 const ExpenseModalDetails = (props: ExpenseModalDetailsPropType) => {
     const { expenseId, expense, userRole, getSingleExpense } = props;
@@ -35,27 +37,40 @@ const ExpenseModalDetails = (props: ExpenseModalDetailsPropType) => {
         });
 
         return (
-            <Box className={styles.metaSection} sx={{ marginTop: '30px' }}>
-                <Typography variant="body2">
-                    <span className={`text-normal ${styles.label}`}>Expense time:</span>{' '}
-                    {formattedExpenseAt}
-                </Typography>
-                <Typography variant="body2">
-                    <span className={`text-normal ${styles.label}`}>Created By:</span> {creatorName}
-                </Typography>
-                <Typography variant="body2">
-                    <span className={`text-normal ${styles.label}`}>DO info:</span>{' '}
-                    {expense.do?.shopName ? expense.do.shopName : 'No DO attached'}
-                </Typography>
-                {userRole !== 'user' && (
-                    <Typography variant="body2">
-                        <span className={`text-normal ${styles.label}`}>Edit history Info: </span>
-                        edited {editCount} time{editCount > 1 ? 's' : ''}
-                    </Typography>
-                )}
-                <Typography variant="body2" className={`text-normal ${styles.label}`}>
-                    {expense.attachmentURL ? 'One attachment' : 'No attachment'}
-                </Typography>
+            <Box className={styles.metaSection} sx={{ marginTop: isLoading ? '60px' : '40px' }}>
+                <ShimmerCardLoader loading={isLoading} line={5}>
+                    <div>
+                        <Typography variant="body2" className={styles.metaRow}>
+                            <span className={`text-normal ${styles.label}`}>Expense time:</span>
+                            <span>{formattedExpenseAt}</span>
+                        </Typography>
+                        <Typography variant="body2" className={styles.metaRow}>
+                            <span className={`text-normal ${styles.label}`}>Created By:</span>
+                            <span>{creatorName}</span>
+                        </Typography>
+                        <Typography variant="body2" className={styles.metaRow}>
+                            <span className={`text-normal ${styles.label}`}>DO info:</span>
+                            <span>
+                                {expense.do?.shopName ? expense.do.shopName : 'No DO attached'}
+                            </span>
+                        </Typography>
+                        {userRole !== 'user' && (
+                            <Typography variant="body2" className={styles.metaRow}>
+                                <span className={`text-normal ${styles.label}`}>
+                                    Edit history Info:
+                                </span>
+                                <span>
+                                    edited {editCount} time{editCount !== 1 ? 's' : ''}
+                                </span>
+                            </Typography>
+                        )}
+                        <Typography variant="body2" className={styles.metaRow}>
+                            <span className={`text-normal ${styles.label}`}>
+                                {expense.attachmentURL ? 'One attachment' : 'No attachment'}
+                            </span>
+                        </Typography>
+                    </div>
+                </ShimmerCardLoader>
             </Box>
         );
     };
@@ -64,22 +79,25 @@ const ExpenseModalDetails = (props: ExpenseModalDetailsPropType) => {
         loadExpenseData();
     }, []);
 
-    if (isLoading) {
-        return <Typography>Loading data</Typography>;
-    }
-
     if (expense === null) {
         return <Typography>No expense found</Typography>;
     }
 
     return (
         <Box className={styles.container}>
-            <Typography variant="h5" component="h2" className={`text ${styles.title}`}>
-                {expense.title || 'Untitled Expense'}
-            </Typography>
-            <Typography variant="body1" className={`text-normal ${styles.detailsDescription}`}>
-                • {expense.description || 'No description provided.'}
-            </Typography>
+            <ShimmerCardLoader loading={isLoading} line={2} hasTitle>
+                <div>
+                    <Typography variant="h5" component="h2" className={`text ${styles.title}`}>
+                        {expense.title || 'Untitled Expense'}
+                    </Typography>
+                    <Typography
+                        variant="body1"
+                        className={`text-normal ${styles.detailsDescription}`}
+                    >
+                        • {expense.description || 'No description provided.'}
+                    </Typography>
+                </div>
+            </ShimmerCardLoader>
             {renderInfoSection()}
         </Box>
     );
